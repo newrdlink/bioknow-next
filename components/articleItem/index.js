49d7eb9a972f6188f218d1c03510d32b139b6a48
Roman Didukh <newrdlink@yandex.ru>
2022-05-23 20:59:32 +0300

    fix articles

--- components/articleItem/index.js
+++ components/articleItem/index.js
@@ -4,28 +4,31 @@ import Link from 'next/link'
 const ArticleItem = ({ id, attributes }) => {
   const { title, subtitle, createdAt, mainImage } = attributes
 
-  //  const date = new Date(createdAt)
-  //console.log(mainImage)
+  const date = new Date(createdAt)
+  // console.log(createdAt)
+  const month = date.getMonth()
+  const day = date.getDate()
+  const year = date.getFullYear()
 
   return (
     <li key={id} className={styles.articalItem}
-    // style={{
-    //   // background: `no-repeat url(${mainImage}) center/cover`,
-    //   // backgroundRepeat: 'no-repeat',
-    //   // backgroundSize: 'cover',
-    //   // opacity: '.4'
+      style={{
+        // background: `no-repeat url(${mainImage}) center/cover`,
+        // backgroundRepeat: 'no-repeat',
+        // backgroundSize: 'cover',
+        // opacity: '.4'
 
-    //   // backgroundImage: `url(${mainImage})`,
-    //   // backgroundRepeat: 'no-repeat',
-    //   // backgroundSize: 'cover',
-    //   // opacity: '.4'
-    // }}
+        // backgroundImage: `url(${mainImage})`,
+        // backgroundRepeat: 'no-repeat',
+        // backgroundSize: 'cover',
+        // opacity: '.4'
+      }}
     >
       <p className={styles.articalItem__num}>{id}</p>
       <Link href={`/articles/${id}`}><a className={styles.articalItem__title}>{title}</a></Link>
       <p className={styles.articalItem__subtitle}>{subtitle}</p>
       <p className={styles.articalItem__date}>
-        {/* {`${date.getDate()} / ${date.getMonth() + 1} / ${date.getFullYear()}`} */}1
+        {`${day} / ${month} / ${year}`}
       </p>
     </li>
   )
