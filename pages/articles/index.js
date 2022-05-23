f9827ed8c0b268e2196b80b35f8ac7f7bcb089ec
Roman Didukh <newrdlink@yandex.ru>
2022-05-23 23:51:44 +0300

    fix articles

--- pages/articles/index.js
+++ pages/articles/index.js
@@ -1,28 +1,24 @@
 import styles from './style.module.scss'
-import api from '../../utils/ApiArtcles'
+import api from 'utils/ApiArtcles'
 import { ArticleItem, HeadPage, titles } from 'components'
+import { metaInfo } from 'constants/metaInfo'
 
 const Articles = ({ articles = [] }) => {
-  // console.log(arr[0].id)
+
   return (
     <section className={styles.articles}>
+      <HeadPage {...metaInfo.find(el => el.page === 'articles')} />
       <titles.TitleBlock
         title="Мои статьи"
         place="articles"
       />
       <ul className={styles.articles__items}>
-        <HeadPage
-          title="Мои статьи по биологии"
-          description="Страница с авторскими статьями"
-          keywords="Статьи по биологии, подготовка к экзамену по биологии, репетитор по биологии"
-        />
         {articles.map((el) => <ArticleItem key={el.id} {...el} />)}
-        {/* {articles.map((el) => <p key={el.id} {...el}>{el.id}</p>)} */}
       </ul>
     </section>
   )
 }
-// it works or it is good case
+
 export async function getServerSideProps() {
 
   const res = await api.getArticles()
