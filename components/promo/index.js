52fe8710684f2562d124b69c07d1348bc5f7bfd7
Roman Didukh <newrdlink@yandex.ru>
2022-05-25 22:22:38 +0300

    add info about

--- components/promo/index.js
+++ components/promo/index.js
@@ -1,7 +1,8 @@
 import styles from './style.module.scss'
 import Image from 'next/image'
 import Link from 'next/link'
-import { titles } from '/components'
+import { titles } from 'components'
+import { aboutInfo } from '../../constants/about'
 
 const Promo = () => {
 
@@ -9,17 +10,17 @@ const Promo = () => {
     <section className={styles.promo}>
       <div className={styles.promo__content}>
         <titles.TitleBlock
-          title="Easy to Use Cloud Budget Management Software"
+          title={aboutInfo.title}
           place="promo"
         />
         <titles.SubtitleBlock
-          subtitle="Our software is made so you can access and manage your budget and expenses online at any time from any device. It provides detailed income and expense reports with graphs so you can easilly see your spending patterns and budget at a glance. Read below to find out more."
+          subtitle={aboutInfo.subtitle.slice(0, 145)}
           place="promo"
         />
-        <Link href="/about"><a className={styles.promo__link}>Подробнее...</a></Link>
+        <Link href="/about"><a className={styles.promo__link}>Далее ...</a></Link>
       </div>
       <div className={styles.promo__img}>
-        <Image layout='fill' src={"https://avatars.mds.yandex.net/i?id=2a0000017a0010be9d446030d120daa09bfb-3948701-images-thumbs&n=13"} alt="Фотография автора" />
+        <Image layout='fill' src={aboutInfo.image} alt="Фотография автора" />
       </div>
     </section>
   )
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,3 +1,24 @@
-const About = () => <p>О себе</p>
+import styles from './style.module.scss'
+import { aboutInfo } from "constants/about"
+import { titles } from 'components'
+
+const About = () => {
+
+  const arr = aboutInfo.subtitle.split('    ')
+  console.log(arr)
+
+
+
+  return (
+    <section className={styles.about}>
+      <titles.TitleBlock
+        title={aboutInfo.title}
+        place="about"
+      />
+      {arr.map((el) => <p className={styles.about__subtitle} key={el}>{el}</p>)}
+
+    </section>
+  )
+}
 
 export default About
