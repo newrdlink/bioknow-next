842a3e38f21241de1493395792d4d02ae7cc1aaa
Roman Didukh <newrdlink@yandex.ru>
2022-07-03 20:35:12 +0300

    add media for 380px

--- components/header/index.js
+++ components/header/index.js
@@ -4,15 +4,13 @@ import { WidthScreen } from 'utils/widthScreen'
 
 
 const Header = () => {
-  // console.log(WidthScreen())
-
   // console.log(WidthScreen().width> 450)
   return (
-      <header className={styles.header}>
-        <Logo />
-        { WidthScreen().width > 450 && <Nav /> }
-        <Contacts />
-      </header>
+    <header className={styles.header}>
+      <Logo />
+      {WidthScreen().width > 450 && <Nav />}
+      <Contacts />
+    </header>
   )
 }
 
